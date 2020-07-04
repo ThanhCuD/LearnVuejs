008120d93943ee106fb2fde731e214419579cf5f
ThanhCuD <thanhduc552018@gmail.com>
2020-07-04 17:11:36 +0700

    add new structure

---
+++ BackEnd/Plife.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Plife.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ BackEnd/Plife.Global/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Plife.Global
+{
+    public class Class1
+    {
+    }
+}
